71c0e195c6872093a190c357a4943faf97e4ad94
Tonyjlum <tonyjlum@gmail.com>
2019-07-28 15:36:37 -0400

    item controller

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -1,2 +1,31 @@
 class ItemController < ApplicationController
+
+  def index
+    @items = Items.all
+    render json: @items
+  end
+
+  def show
+    @item = Items.find(params[:id])
+    render json: @item
+  end
+
+  def create
+    @item = Items.create(item_params)
+    render json: @item
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    render json: @item
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @deleted_item = @item
+    @item.destroy
+    render json: @deleted_item
+  end
+
 end
